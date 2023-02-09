9ce7ce5303350b2d7fd53c0cfd762c12d85208b0
Kenny Brast <kenny.brast@gmail.com>
2023-02-09 20:13:49 +0000

    Using Python String Methods

---
+++ variations.py
@@ -0,0 +1,17 @@
+# Using Python String Methods
+
+message = input("Enter a message: ")
+
+print("Lowercase:", message.lower())
+print("Uppercase:", message.upper())
+print("Capitalized:", message.capitalize())
+print("Title Case:", message.title())
+
+words = message.split()
+print("Words:", words)
+
+sorted_words = sorted(words)
+print("Alphabetic First Word:", sorted_words[0])
+print("Alphabetic Last Word:", sorted_words[-1])
+
+
