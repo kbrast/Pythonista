a2055807e0b6e36bef1ed417f1826777249d5f41
Kenny Brast <kenny.brast@gmail.com>
2023-02-10 13:07:47 +0000

    Defining and Using Python Generators

---
+++ using-generators.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Defining and Using Python Generators
+
+def char_range(start, stop, step=1):
+    stop_modifier = 1
+    start_code = ord(start)
+    stop_code = ord(stop)
+
+    if start_code > stop_code:
+        step *= -1
+        stop_modifier *= -1
+
+    for value in range(start_code, stop_code + stop_modifier, step):
+        yield chr(value)
