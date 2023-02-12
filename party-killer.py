496ef41e5cf9fd10cec42f7b31531e58515129eb
Kenny Brast <kenny.brast@gmail.com>
2023-02-12 17:16:40 -0600

    open a new browser tab to a specific web page

---
+++ party-killer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# party-killer.py
+
+import webbrowser
+
+# Open YouTube and play Bread’s greatest hits
+webbrowser.open_new_tab("https://www.youtube.com/watch?v=lwkgjz2tpqk&t=0")
